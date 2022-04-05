b00d882832835c628db00db2d10d4012ee1d9951
Ahmad Elshowair <it.is.work@outlook.com>
2022-04-05 14:13:30 +0700

    add text to an image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -21,31 +21,52 @@ export const Meme = ()=>{
    };
 
 
+   const handleChange = (event)=>{
+      const {name, value} = event.target;
+      setMeme((prevMeme) =>{
+         return {
+           ...prevMeme,
+           [name]: value
+         };
+      });
+   };
     return(
-       <main>
+       <main className='main'>
          <div className="form">
-         <input 
-            type="text" 
-            placeholder="top text" 
-            className="control--form"
-         />
-         <input 
-            type="text" 
-            placeholder="bottom text" 
-            className="control--form"
-         />
+            <input 
+               type="text" 
+               placeholder="top text" 
+               className="control--form"
+               name='topText'
+               value={meme.topText}
+               onChange={handleChange}
+            />
+            <input 
+               type="text" 
+               placeholder="bottom text" 
+               className="control--form"
+               name='bottomText'
+               value={meme.bottomText}
+               onChange={handleChange}
+            />
 
-         <button 
-            type="submit" 
-            className="form--btn"
-            onClick={getMemeImage}
-         >
-            Get a new meme image
-         </button>
-         <div className='meme'>
-            <img src={meme.randomImage} alt="meme pic" className='image--meme'/>
-         </div>
+            <button 
+               type="submit" 
+               className="form--btn"
+               onClick={getMemeImage}
+            >
+               Get a new meme image
+            </button>
          </div> 
+         <div className='meme'>
+            <img 
+               src={meme.randomImage} 
+               alt="meme pic" 
+               className='image--meme'
+            />
+            <h2 className='meme--text top'>{meme.topText}</h2>
+            <h2 className='meme--text bottom'>{meme.bottomText}</h2>
+         </div>
        </main>
     ); 
  };
