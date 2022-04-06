5d1741a808fcb3ede2b81954fbe335ca6df6e116
Ahmad Elshowair <it.is.work@outlook.com>
2022-04-06 23:37:01 +0700

    update Read me

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ()=>{
                 <img src={logo} className='logo__image' alt='troll face'/>
                 <p className='logo__text'>Meme Generator</p>
             </div>
-            <h3 className='project__name'>React Course - Project 3</h3>
+            <h3 className='project__name'>Project 3</h3>
         </nav>
     );
 };
